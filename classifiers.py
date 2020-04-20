98d9ec3dcc7d955b3ba235c7616e3cb244ae1972
hal <halsrrockwell@gmail.com>
2020-04-19 20:09:00 -0400

    added basic pixel-wise classification and testing
    Haven't run the ClassifyTest FSM on the robot yet, but I think it should
    work (will do that tomorrow morning). Basic idea is we can just swap out
    different algorithms in a line or two, and see how they work (and do the
    same thing in our final occupancy-grid-building code).

---
+++ classifiers.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class PatchClassifier():
+    """
+    Base class for patch classifier, just laying out the abstract structure.
+    """
+    def __init__(self):
+        pass # might later add things here, if there are commonalities
+
+    def __call__(self, patch):
+        raise NotImplementedError # true for floor, false otherwise
+
+class PixelClassifier(PatchClassifier):
+    """
+    Patch classification with simple MSE on pixels.
+    """
+    def __init__(self, patches, threshold=None):
+        """
+        patches: NxHxWxC numpy array of RGB image patches
+        threshold: threshold for SSE, optional, determined automatically if
+                not specified
+        """
+        assert isinstance(patches, np.ndarray) and len(patches.shape) == 4
+
+        self.patches = patches
+        
+        if threshold is not None:
+            self.threshold = threshold
+        else:
+            # set threshold to try to include all patches
+            mses = [np.mean((patch - patches)**2) for patch in patches]
+            self.threshold = np.max(mses) * 1.05 # wiggle room
+
+    def __call__(self, patch):
+        assert patch.shape == self.patch[0].shape
+
+        return np.mean((patch - self.patches)**2) < self.threshold
