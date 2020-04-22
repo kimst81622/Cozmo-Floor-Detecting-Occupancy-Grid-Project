1b58b1794b6bb5f3ae5b8d9b473bb1c0f3e7131f
hal <halsrrockwell@gmail.com>
2020-04-22 10:50:06 -0400

    fixes from monday, and my saved wooden floor
    Some fixes to ClassifyTest and classifiers to make them work
    for the demo (and adding the HueClassifier to classifiers).
    Also made a directory we can start saving floors in, so we don't
    have to recollect every time.

--- classifiers.py
+++ classifiers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 class PatchClassifier():
     """
@@ -16,7 +17,7 @@ class PixelClassifier(PatchClassifier):
     """
     def __init__(self, patches, threshold=None):
         """
-        patches: NxHxWxC numpy array of RGB image patches
+        patches: NxHxWxC numpy array of BGR image patches
         threshold: threshold for SSE, optional, determined automatically if
                 not specified
         """
@@ -35,3 +36,37 @@ class PixelClassifier(PatchClassifier):
         assert patch.shape == self.patches[0].shape
 
         return np.mean((patch - self.patches)**2) < self.threshold
+
+class HueClassifier(PatchClassifier):
+    """
+    Patch classification with simple squared distance of average hue.
+    """
+    def __init__(self, patches, threshold=None):
+        """
+        patches: NxHxWxC numpy array of BGR image patches
+        threshold: optional threshold for distance, determined automatically if
+                not specified
+        """
+        assert isinstance(patches, np.ndarray) and len(patches.shape) == 4
+
+        self.patches = np.stack([cv2.cvtColor(patch, cv2.COLOR_BGR2HSV)
+            for patch in patches])
+        # only average hue for each patch needed
+        print(self.patches.shape)
+        self.patches = self.patches.mean((1,2))[:, 0]
+        
+        if threshold is not None:
+            self.threshold = threshold
+        else:
+            # set threshold to try to include all patches
+            mses = [np.mean((patch - self.patches)**2) 
+                    for patch in self.patches]
+            self.threshold = np.quantile(mses, 0.9) * 1.05 # wiggle room
+            print(f'choosing threshold of {self.threshold}')
+
+    def __call__(self, patch):
+        hue = cv2.cvtColor(patch, cv2.COLOR_BGR2HSV).mean((0,1))[0]
+        dist = np.mean((hue - self.patches)**2)
+        print(f'distance of patch: {dist}')
+
+        return dist < self.threshold
